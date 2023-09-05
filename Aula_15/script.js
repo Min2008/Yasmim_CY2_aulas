119e6025d180f7e0cf33983ec254678f201401bc
Min2008 <131827410+Min2008@users.noreply.github.com>
2023-09-05 15:39:32 -0300

    dever da aula 15

--- Aula_15/script.js
+++ Aula_15/script.js
@@ -1,2 +1,13 @@
-var mensagem = document.getElementById('msg')
-var img = document.getElementById('foto')
+let horario = window.prompt('digite as horas: ')
+
+if (horario <= 12){
+    var mensagem = document.getElementById('msg')
+    var img = document.getElementById('foto')
+}
+
+// if (horario > 12 && horario <= 19){
+// }
+
+// if (horario > 19 && horario <= 11){
+
+// }
