e203800c1c7a115259c7a37736d5a01a1e998fa3
Min2008 <131827410+Min2008@users.noreply.github.com>
2023-09-05 20:20:40 -0300

    aula 15 e 16

--- Aula_15/script.js
+++ Aula_15/script.js
@@ -1,13 +1,39 @@
-let horario = window.prompt('digite as horas: ')
+var mensagem = document.getElementById('msg')
+var img = document.getElementById('foto')
+var horas = document.getElementById('horas')
+let horario = prompt('digite as horas: ')
+var data = new Date();
+var hora = data.getHours()
 
-if (horario <= 12){
-    var mensagem = document.getElementById('msg')
-    var img = document.getElementById('foto')
-}
+horas.innerHTML = `Agora são ${horario} horas`
+
+
+// if (horario <= 12){ eu fiz
+//     var mensagem = document.getElementById('msg')
+//     var img = document.getElementById('foto')
+// }
 
 // if (horario > 12 && horario <= 19){
 // }
 
 // if (horario > 19 && horario <= 11){
+// } eu fiz
 
-// }
+// correção: =>
+
+if (horario >= 5 && horario <= 12){
+    document.body.style.background = 'brown'
+    mensagem.innerHTML = 'BOM DIA!!!'
+
+} 
+else if (horario >= 12 && horario <= 18){
+    document.body.style.background = 'lightblue'
+    mensagem.innerHTML = 'BOA TARDE!!!'
+    img.src = './img/boa tarde.jpg'
+}
+else{
+    document.body.style.background = 'purple'
+    mensagem.innerHTML = 'BOA NOITE!!!'
+    img.src = './img/boa noite.jpg'
+}
+
