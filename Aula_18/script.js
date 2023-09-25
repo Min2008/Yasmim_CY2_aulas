494bb88529d4a7965d54e0941739ee71eaa7de09
Min2008 <131827410+Min2008@users.noreply.github.com>
2023-09-25 14:21:57 -0300

    boletim escolar com prompt

--- Aula_18/script.js
+++ Aula_18/script.js
@@ -1,5 +1,21 @@
-// let nome = prompt('Qual o seu nome?')
-// let nota1 = prompt('Nota 1: ')
-// let nota2 = prompt('Nota 2: ')
-// let nota3 = prompt('Nota 3: ')
+variaveis
+let aluno = prompt('Qual o seu nome?')
+let nota1 = prompt('Nota 1: ')
+let nota2 = prompt('Nota 2: ')
+let nota3 = prompt('Nota 3: ')
+// var nome = document.getElementById('nome')
+// var resultado = document.getElementById('resultado')
+// var media = document.getElementById('media')
+
+// var media_total = new Mediat(nota1, nota2, nota3);
+// var total = media_total.getMediaTotal();
+
+
+
+
+if (total < 6){
+    resultado.innerHTML = ' Você foi REPROVADO!'
+} else{
+    resultado.innerHTML = 'Você foi APROVADO!'
+}
 
