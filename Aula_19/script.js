d6367c8b9eaddfec48dfc8869ef8e09bbc1791f8
Min2008 <131827410+Min2008@users.noreply.github.com>
2023-10-10 12:29:50 -0300

    dever 1010

---
+++ Aula_19/script.js
@@ -0,0 +1,19 @@
+// identificar as variaveis 
+// identificar os botoes usando 
+// buttons.foreach(btn) => juntamente com o addeventlistener faz uma const value para pegar os numeros usando target.innertext
+ // depois embaixo voce coloca para converter o texto em numeros e o resto para fazer outra coisa
+ // if (+value) for os numertos e o ponto senao vai ser uma operaçao
+
+ // construir a calculadora: class calculator() usando o construct que ajuda a diferencias suas caracteristicas de cada operaçao
+ // this.lllll = lllll - prorpiedade do objeto, mais facil de programar e arrumar
+ // this.LLLL = "" - REPRESENTA O QUE A PESSOA ESTA DIGITANDO NA CALCULADORA
+ // mais metodos:
+ // addDigit - metodo dos digitos
+ // dentro dele colocar this.LLLL para saber com é a operaçao clicada
+ // e colocar this.update screen para atualizar a tela
+ // faz  funçao updatescreen nesta funçao vc concatena as LLL.innertext += LLLL 
+ // checagem para o uso do ponto: if digit '.' e LLLL.includes '.' - return;
+ // total das operaçoes - processamento: fazer let total, let previous e let current
+ // para colocar os numeros no previous e fazer a operacao desejada com o currentl
+ // swith(operation) - e fazer um case para cada operacao e um defalt return no final caso seja invalida
+ // APLICAR OS METODOS USANDO const e new.calculator
