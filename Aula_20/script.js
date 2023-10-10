2a859b4e43da74a652021c3dcd6fee4835d2e997
LFabreu <131827396+LFabreu@users.noreply.github.com>
2023-10-10 20:26:07 -0300

    começando o gerador de senhas

---
+++ Aula_20/script.js
@@ -0,0 +1,23 @@
+const senha_elemet = document.getElementById("senha");
+const copiar_elemet = document.getElementById("copiar");
+const tamanho_elemet = document.getElementById("tamanho");
+const maiusculas_elemet = document.getElementById("maiusculas");
+const minusculas_elemet = document.getElementById("minusculas");
+const numeros_elemet = document.getElementById("numeros");
+const caracteres_elemet = document.getElementById("caracteres-especiais");
+const gerar_senha_elemet = document.getElementById("gerar-senha");
+
+const maiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+const minusculas = 'abcdefghijklmnopqrstuvwxyz'
+const numeros = '0123456789'
+const caracteres_especiais = '+-*/!#=$@%&`?.,;ç'
+
+function gerar_senha(){
+    let tamanho_senha = tamanho_elemet.value
+    
+}
+
+
+
+
+
