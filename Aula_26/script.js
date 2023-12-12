673294de316a7c323acfd4fae779fea0ea7567aa
Min2008 <131827410+Min2008@users.noreply.github.com>
2023-12-12 20:17:57 -0300

    terminado css e quase acabou o java

--- Aula_26/script.js
+++ Aula_26/script.js
@@ -1,2 +1,51 @@
-const URL_BASE = 'https://docs.awesomeapi.com.br/api-cep'
+// const URL_BASE = 'https://docs.awesomeapi.com.br/api-cep'
+
+const CEP = document.querySelector('.cep')
+const LOGRADOURO = document.querySelector('.logradouro')
+const ESTADO = document.querySelector('.estado')
+const CIDADE = document.querySelector('.cidade')
+const BAIRRO = document.querySelector('.bairro')
+const FORM = document.querySelector('.cep-conteiner')
+
+const fetch_cep = async(cep) => {
+    api_response = await fetch(`https://cep.awesomeapi.com.br/json/${cep}`)
+    if(api_response.status === 200){
+        const endereco = api_response.json()
+        return endereco
+    }
+}
+
+const render_cep = async(cep) => {
+    const informacao = await fetch_cep(cep)
+    if(informacao){
+        console.log(informacao)
+        CEP.value = informacao.cep
+        LOGRADOURO.value = informacao.address
+        ESTADO.value = informacao.state
+        CIDADE.value = informacao.city
+        BAIRRO.value = informacao.district
+    }
+    else{
+        LOGRADOURO.innerHTML = "none"
+        ESTADO.innerHTML = "none"   
+        CIDADE.innerHTML = "none"
+        BAIRRO.innerHTML = "none"
+    }
+}
+
+FORM.addEventListener('submit', (evento) =>{
+    
+    let ceps = CEP.value
+    render_cep(ceps)
+});
+
+
+
+
+
+
+
+
+
+
 
