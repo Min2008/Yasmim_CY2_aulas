99ddd6f9be6418148a31f25c083ba2387ffbfa72
Min2008 <131827410+Min2008@users.noreply.github.com>
2024-01-11 13:44:43 -0300

    calculadora ferias

--- Aula_19/script.js
+++
@@ -1,7 +0,0 @@
-// identificar as variaveis 
-const OPANT = document.querySelector("#operacoes-anteriores")
-const OPPS = document.querySelector("#operacoes-pessoa")
-// identificar os botoes usando 
-const BUTTONS = document.querySelectorAll("#conteiner_operacoes button")
-
-
